755353b1909a9f18e8edbd3aae9faf22d6115c3e
Patrick José Pereira <patrickelectric@gmail.com>
2021-04-07 13:47:03 -0300

    wip
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ src/features/system.rs
@@ -0,0 +1,42 @@
+use sysinfo::{RefreshKind, System, SystemExt};
+
+pub enum SystemType {
+    Components,
+    Cpu,
+    Disks,
+    DisksList,
+    Everything,
+    Memory,
+    Networks,
+    Processes,
+    UsersList,
+}
+
+pub fn generate_serde_value(system_type: SystemType) {
+    let mut system = System::new();
+
+    match system_type {
+        Components => {
+            system.refresh_components();
+            system.refresh_components_list();
+            for component in system.get_components() {
+                println!("{:#?}", component);
+            }
+        }
+
+        Cpu => {
+            system.refresh_cpu();
+            for component in system.get_() {
+                println!("{:#?}", component);
+            }
+        }
+
+        Networks => {
+            system.refresh_networks();
+            system.refresh_networks_list();
+            for network in system.get_networks() {
+                println!("{:#?}", network);
+            }
+        }
+    }
+}
