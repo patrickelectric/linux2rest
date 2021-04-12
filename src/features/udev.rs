6cdfdf79c4fe5bcf22ff561d1e9b368baad0ee8a
Patrick José Pereira <patrickelectric@gmail.com>
2021-04-12 15:53:42 -0300

    features: udev: Improve device serialization
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/features/udev.rs
+++ src/features/udev.rs
@@ -37,44 +37,6 @@ impl<'a> From<&'a udev::Device> for DeviceUdevAttributes<'a> {
     }
 }
 
-impl<'a> Serialize for GenericSerializer<'a, udev::Device> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut state = serializer.serialize_struct("DeviceUdev", 12)?;
-        state.serialize_field("initialized", &self.0.is_initialized())?;
-        state.serialize_field("device_major_minor_number", &self.0.devnum())?;
-        state.serialize_field("system_path", &self.0.syspath())?;
-        state.serialize_field("device_path", &self.0.devpath().to_str())?;
-        state.serialize_field(
-            "device_node",
-            &self.0.devnode().and_then(|value| value.to_str()),
-        )?;
-        state.serialize_field(
-            "subsystem_name",
-            &self.0.subsystem().and_then(|value| value.to_str()),
-        )?;
-        state.serialize_field("system_name", &self.0.sysname().to_str())?;
-        state.serialize_field("instance_number", &self.0.sysnum())?;
-        state.serialize_field(
-            "device_type",
-            &self.0.devtype().and_then(|value| value.to_str()),
-        )?;
-        state.serialize_field("driver", &self.0.driver().and_then(|value| value.to_str()))?;
-        state.serialize_field("action", &self.0.action().and_then(|value| value.to_str()))?;
-        if let Some(parent) = &self.0.parent() {
-            state.serialize_field("parent", &GenericSerializer::from(parent))?;
-        } else {
-            let x: Option<&str> = None;
-            state.serialize_field("parent", &x)?;
-        }
-        state.serialize_field("properties", &DeviceUdevProperties::from(self.0))?;
-        state.serialize_field("attributes", &DeviceUdevAttributes::from(self.0))?;
-        state.end()
-    }
-}
-
 impl Serialize for DeviceUdevProperties<'_> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
@@ -123,8 +85,30 @@ pub fn generate_serde_value() -> Vec<serde_json::Value> {
     let mut enumerator = udev::Enumerator::new().unwrap();
     let result = enumerator.scan_devices().unwrap();
     let vector: Vec<serde_json::Value> = result
-        .map(|device| serde_json::to_value(&GenericSerializer::from(&device)).unwrap())
+        .map(|device| generate_serde_from_device(&device))
         .collect();
 
     return vector;
+}
+
+fn generate_serde_from_device(device: &udev::Device) -> serde_json::Value {
+    serde_json::json!({
+        "initialized": device.is_initialized(),
+        "device_major_minor_number": device.devnum(),
+        "system_path": device.syspath(),
+        "device_path": device.devpath().to_str(),
+        "device_node": device.devnode().and_then(|value| value.to_str()),
+        "subsystem_name": device.subsystem().and_then(|value| value.to_str()),
+        "system_name": device.sysname().to_str(),
+        "instance_number": device.sysnum(),
+        "device_type": device.devtype().and_then(|value| value.to_str()),
+        "driver": device.driver().and_then(|value| value.to_str()),
+        "action": device.action().and_then(|value| value.to_str()),
+        "parent": match device.parent() {
+            None => serde_json::json!(null),
+            Some(parent) => generate_serde_from_device(&parent)
+        },
+        "properties": &DeviceUdevProperties::from(device),
+        "attributes": &DeviceUdevAttributes::from(device),
+    })
 }
