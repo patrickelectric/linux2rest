ae44b16257850926c7042261368df68ecb49c2ce
Patrick José Pereira <patrickelectric@gmail.com>
2021-04-12 16:52:20 -0300

    server: Add first version
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ src/server/mod.rs
@@ -0,0 +1,24 @@
+mod pages;
+
+use actix_web::{rt::System, web, App, HttpServer};
+
+// Start REST API server with the desired address
+pub fn run(server_address: &str) {
+    let server_address = server_address.to_string();
+
+    // Start HTTP server thread
+    let system = System::new("http-server");
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(pages::root))
+            .route(r"/{filename:.*(\.html|\.js)}", web::get().to(pages::root))
+            .route("/netstat", web::get().to(pages::netstat))
+            .route("/system", web::get().to(pages::system))
+            .route("/udev", web::get().to(pages::udev))
+    })
+    .bind(server_address)
+    .unwrap()
+    .run();
+
+    let _ = system.run();
+}
---
+++ src/server/pages.rs
@@ -0,0 +1,63 @@
+use actix_web::{web, HttpRequest, HttpResponse};
+use log::*;
+
+use crate::features;
+
+pub fn load_file(file_name: &str) -> String {
+    // Load files at runtime only in debug builds
+    if cfg!(debug_assertions) {
+        use std::io::prelude::*;
+        let html_path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("src/html/");
+        let mut file = std::fs::File::open(html_path.join(file_name)).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        return contents;
+    }
+
+    match file_name {
+        "" | "index.html" => r#"std::include_str!("../html/index.html").into()"#.into(),
+        "vue.js" => r#"std::include_str!("../html/vue.js").into()"#.into(),
+        _ => format!("File not found: {}", file_name),
+    }
+}
+
+pub fn root(req: HttpRequest) -> HttpResponse {
+    let path = match req.match_info().query("filename") {
+        "" | "index.html" => load_file("index.html"),
+        "vue.js" => load_file("vue.js"),
+        something => {
+            //TODO: do that in load_file
+            return HttpResponse::NotFound()
+                .content_type("text/plain")
+                .body(format!("Page does not exist: {}", something));
+        }
+    };
+    HttpResponse::Ok().content_type("text/html").body(path)
+}
+
+pub fn netstat(req: HttpRequest) -> HttpResponse {
+    debug!("{:#?}", req);
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(serde_json::to_string_pretty(&features::netstat::generate_serde_value()).unwrap())
+}
+
+pub fn system(req: HttpRequest) -> HttpResponse {
+    debug!("{:#?}", req);
+
+    HttpResponse::Ok().content_type("application/json").body(
+        serde_json::to_string_pretty(&features::system::generate_serde_value(
+            features::system::SystemType::Everything,
+        ))
+        .unwrap(),
+    )
+}
+
+pub fn udev(req: HttpRequest) -> HttpResponse {
+    debug!("{:#?}", req);
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(serde_json::to_string_pretty(&features::udev::generate_serde_value()).unwrap())
+}
