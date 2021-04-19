40821b84ef0d55ea5448d1b35da8ac80d6cf8c5f
Patrick José Pereira <patrickelectric@gmail.com>
2021-04-19 16:13:29 -0300

    Turn raspberry as a platform feature
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/features/platform.rs
+++ src/features/platform.rs
@@ -1,12 +1,22 @@
 use log::*;
-use rppal;
 
 pub fn generate_serde_value() -> serde_json::Value {
-    match rppal::system::DeviceInfo::new() {
-        Ok(system) => serde_json::json!({
-            "model": system.model().to_string(),
-            "soc": system.soc().to_string(),
-        }),
-        Err(error) => serde_json::json!({ "error": format!("{:?}", error) }),
+    #[cfg(feature = "raspberry")]
+    {
+        use rppal;
+        return match rppal::system::DeviceInfo::new() {
+            Ok(system) => serde_json::json!({
+                "raspberry": {
+                    "model": system.model().to_string(),
+                    "soc": system.soc().to_string(),
+                }
+            }),
+            Err(error) => serde_json::json!({ "error": format!("{:?}", error) }),
+        };
     }
+
+    #[cfg(not(feature = "raspberry"))]
+    return serde_json::json!({
+        "error": "Unsupported platform."
+    });
 }
