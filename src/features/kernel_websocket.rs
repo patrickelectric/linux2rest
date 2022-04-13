933f95b17b428cf11641b9c1d43e8d40559ca48c
Patrick José Pereira <patrickelectric@gmail.com>
2022-04-13 15:30:01 -0300

    src: features: kernel_websocket: Use kernel over kernel_buffer
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/features/kernel_websocket.rs
+++ src/features/kernel_websocket.rs
@@ -1,5 +1,8 @@
+use crate::features::kernel;
+use actix::prelude::*;
 use actix::{self, Actor, Addr, AsyncContext, Handler, Message, StreamHandler};
 use actix_web_actors::ws;
+use futures::channel::mpsc::Receiver;
 use serde::Serialize;
 
 use log::*;
@@ -22,13 +25,12 @@ pub struct WebsocketError {
     pub error: String,
 }
 
-#[derive(Debug)]
 pub struct WebsocketActorContent {
     pub actor: Addr<WebsocketActor>,
     pub event_type: WebsocketEventType,
 }
 
-#[derive(Debug, Default)]
+#[derive(Default)]
 pub struct WebsocketManager {
     pub clients: Vec<WebsocketActorContent>,
 }
@@ -42,35 +44,21 @@ pub fn manager() -> Arc<Mutex<WebsocketManager>> {
     return SYSTEM.clone();
 }
 
-impl WebsocketManager {
-    pub fn send(&self, event_type: WebsocketEventType, value: &serde_json::Value) {
-        if self.clients.is_empty() {
-            return;
-        }
-
-        let string = serde_json::to_string_pretty(value).unwrap();
-        self.clients
-            .iter()
-            .filter(|client| client.event_type == event_type)
-            .for_each(|client| {
-                client.actor.do_send(StringMessage(string.clone()));
-            });
-    }
+pub fn new_websocket(_event_type: WebsocketEventType) -> WebsocketActor {
+    WebsocketActor::new(SYSTEM.clone())
 }
 
-pub fn new_websocket(event_type: WebsocketEventType) -> WebsocketActor {
-    WebsocketActor::new(SYSTEM.clone(), event_type)
-}
-
-#[derive(Debug)]
 pub struct WebsocketActor {
     server: Arc<Mutex<WebsocketManager>>,
-    event_type: WebsocketEventType,
+    receiver: Option<Receiver<String>>,
 }
 
 impl WebsocketActor {
-    pub fn new(server: Arc<Mutex<WebsocketManager>>, event_type: WebsocketEventType) -> Self {
-        Self { server, event_type }
+    pub fn new(server: Arc<Mutex<WebsocketManager>>) -> Self {
+        Self {
+            server,
+            receiver: Some(kernel::ask_for_client()),
+        }
     }
 }
 
@@ -84,21 +72,20 @@ impl Handler<StringMessage> for WebsocketActor {
 
 impl Actor for WebsocketActor {
     type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        debug!("Starting websocket");
+        ctx.add_stream(self.receiver.take().unwrap());
+    }
+}
+
+impl StreamHandler<String> for WebsocketActor {
+    fn handle(&mut self, data: String, ctx: &mut Self::Context) {
+        ctx.text(data)
+    }
 }
 
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebsocketActor {
-    fn started(&mut self, ctx: &mut Self::Context) {
-        debug!("Starting websocket, add itself in manager.");
-        self.server
-            .lock()
-            .unwrap()
-            .clients
-            .push(WebsocketActorContent {
-                actor: ctx.address(),
-                event_type: self.event_type,
-            });
-    }
-
     fn finished(&mut self, ctx: &mut Self::Context) {
         debug!("Finishing websocket, remove itself from manager.");
         self.server
