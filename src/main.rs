eccc3de85a21adf3f5ba25fcad702c34fae2b2db
Patrick José Pereira <patrickelectric@gmail.com>
2024-03-12 09:35:40 -0300

    main: Use recorder
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ extern crate lazy_static;
 mod cli;
 mod features;
 mod logger;
+mod recorder;
 mod server;
 
 fn main() {
     logger::init();
     features::platform::start();
-
+    recorder::start();
     server::run(&format!("0.0.0.0:{}", cli::args().as_ref().port));
 }
