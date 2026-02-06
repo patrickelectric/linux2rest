7f0da81b2b7849d46c1178dcec496af647913b00
João Antônio Cardoso <joao.maker@gmail.com>
2026-02-05 23:30:48 -0300

    Fix sampling, featuring .. the 'Sampler' (actor)
    This patch ensures no weird values by preventing:
      - on-demand (and non-monotonic) sampling;
      - cache stampede;
      - lock contention.
    
    This is done by implementing the "Sampler" actor and the static SystemInner, so we have:
      - monotonic sampling that honors minimum sampling interval, improving sampling quality;
      - intrinsic 'perfect cache' (zero miss) by architecture, ensuring system responsiveness;
      - zero lock contention, improving overall application responsiveness.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ mod server;
 
 fn main() {
     logger::init();
+
+    features::system::start(std::time::Duration::from_secs(5));
+
     features::platform::start();
     recorder::start();
     server::run(&format!("0.0.0.0:{}", cli::args().as_ref().port));
